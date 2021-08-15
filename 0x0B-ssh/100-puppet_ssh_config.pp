eada8fdd701e5bd2923f559dbb9d55c0a024fbc9
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-08-14 19:25:56 -0500

    PUPPET config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,5 @@
 # Client configuration  file (w/ Puppet)
-file_line { 'config Puppet':
-    path => '~/.ssh/holberton',
-    line => 'IdentityFile ~/.ssh/holberton',
-}
 file_line { 'config Puppet  passwd':
-    path => '~/.ssh/holberton',
+    path => 'bin/ssh/holberton',
     line => 'PasswordAuthentication no',
 }
