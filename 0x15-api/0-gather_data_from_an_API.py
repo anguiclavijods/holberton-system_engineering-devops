97eb4d87e3bd42a5a01947729657193e223f8fb2
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-09-15 18:32:40 -0500

    script that gather data from API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
 """ Script that gather data from an API,
-    For verify data of empleyes """
+    For verify data of employes """
 import requests
 import sys
 
-id_user = (sys.argv[1])
-response_users = requests\
-    .get("https://jsonplaceholder.typicode.com/users/" + str(id_user))
-user = response_users.json()
+if __name__ == "__main__":
 
-response_todo = requests.\
-    get("https://jsonplaceholder.typicode.com/todos?userId=" + str(id_user))
-user_tasks = response_todo.json()
-total_tasks = len(user_tasks)
+    id_user = (sys.argv[1])
+    response_users = requests\
+        .get("https://jsonplaceholder.typicode.com/users/" + str(id_user))
+    user = response_users.json()
 
-url = "https://jsonplaceholder.typicode.com/todos?userId={}&completed=true"\
-    .format(id_user)
-response_todo_completed = requests.get(url)
-todo_completed = response_todo_completed.json()
-total_completed = len(todo_completed)
+    response_todo = requests.get("https://jsonplaceholder"
+                                 ".typicode.com/todos?userId=" + str(id_user))
+    user_tasks = response_todo.json()
+    total_tasks = len(user_tasks)
 
-print("Employee {} is done with tasks ({}):".format(
-    user["name"], str(total_completed) + "/" + str(total_tasks)))
-for user_task in todo_completed:
-    print("\t{}".format(user_task["title"]))
+    url = "https://jsonplaceholder." \
+          "typicode.com/todos?userId={}&completed=true".format(id_user)
+    response_todo_completed = requests.get(url)
+    todo_completed = response_todo_completed.json()
+    total_completed = len(todo_completed)
+
+    print("Employee {} is done with tasks ({}):".format(
+        user["name"], str(total_completed) + "/" + str(total_tasks)))
+    for user_task in todo_completed:
+        print("\t {}".format(user_task["title"]))
