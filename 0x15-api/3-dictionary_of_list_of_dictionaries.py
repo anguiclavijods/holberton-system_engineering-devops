edc03e20e078e87f70684c9af78ab372f36fd646
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-09-17 17:01:40 -0500

    dictionary of dictionaries in API

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,7 +9,24 @@ if __name__ == "__main__":
 
     response_users = requests \
         .get("https://jsonplaceholder.typicode.com/users/")
-    user = response_users.json()
+    users = response_users.json()
 
-with open('todo_all_employees.json', 'w', newline='') as file:
-    json.dump(user, file)
+    response_todos = requests.\
+        get("https://jsonplaceholder.typicode.com/todos")
+    todos = response_todos.json()
+
+    format_task = {}
+    list_task = []
+    format_task_dict = {}
+    for user in users:
+        for todo in todos:
+            if todo.get("userId") == user.get("id"):
+                format_task.update({"username": user.get("username", None)})
+                format_task.update({"task": todo.get("title", None)})
+                format_task.update({"completed": todo.get("completed", None)})
+                list_task.append(dict(format_task))
+        format_task_dict[user.get("id")] = list(list_task)
+        list_task.clear()
+
+    with open('todo_all_employees.json', 'w', newline='') as file:
+        json.dump(format_task_dict, file)
