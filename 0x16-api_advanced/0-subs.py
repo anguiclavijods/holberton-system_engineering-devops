d783769a1277265a5b5a224aac50caff9efa9119
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-09-27 21:24:02 -0500

    how many subsus... of reddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" script that verify in the api number of subscriptors of reddit"""
+""" script that verify in the api number of subscriptors of reddit """
 import requests
 from urllib import response
 
 
 def number_of_subscribers(subreddit):
-    """ function number of subscribers for verify url and data of users"""
+    """ function number of subscribers for verify url and data of users """
     all_info_reddit = requests.get(
                                    "https://www.reddit.com/r/{}/about.json"
                                    .format(subreddit),
@@ -13,6 +13,6 @@ def number_of_subscribers(subreddit):
                                    allow_redirects=False)
 
     if all_info_reddit == 200:
-        return response.json().get("data").get("subscribers")
+        return (response.json().get("data").get("subscribers"))
     else:
         return 0
