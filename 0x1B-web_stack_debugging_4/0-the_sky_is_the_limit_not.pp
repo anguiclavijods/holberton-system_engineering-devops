5fd0aa2886a006c2099663b9ce88e507444360ca
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-10-05 12:42:29 -0500

    Apche Beanch used for debuggear the stack and view request failed and as fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,10 +2,10 @@
 # used Apache Beanch and the stack
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
+  path    => '/usr/local/bin/:/bin/',
+}
 
 exec { 'nginx-restart':
   command => 'nginx restart',
-  path    => '/etc/init.d/'
+  path    => '/etc/init.d/',
 }
